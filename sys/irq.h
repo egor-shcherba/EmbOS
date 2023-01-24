8e9d29e32ff3fc1c5f93da6ed3534258965f3e0f
Egor Shcherba <egorshcherba94@gmail.com>
2023-01-24 11:07:04 +0300

    irq.h:: define global inerrupt request methods

---
+++ sys/irq.h
@@ -0,0 +1,21 @@
+#ifndef _TRAPS_H
+#define _TRAPS_H
+
+extern void irq_0(void);
+extern void irq_1(void);
+extern void irq_2(void);
+extern void irq_3(void);
+extern void irq_4(void);
+extern void irq_5(void);
+extern void irq_6(void);
+extern void irq_7(void);
+extern void irq_8(void);
+extern void irq_9(void);
+extern void irq_10(void);
+extern void irq_11(void);
+extern void irq_12(void);
+extern void irq_13(void);
+extern void irq_14(void);
+extern void irq_15(void);
+
+#endif /* NOT _TRAPS_H */
