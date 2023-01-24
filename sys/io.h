841fd29eb2acc2191e0a3cb3e41383a9419b4c25
Egor Shcherba <egorshcherba94@gmail.com>
2023-01-24 10:23:10 +0300

    io.h:: inb replace '=r' to '=a'

--- sys/io.h
+++ sys/io.h
@@ -6,7 +6,7 @@
 static inline void
 outb(uint16_t port, uint8_t data)
 {
-  __asm__ volatile ("out %0, %1" :: "a" (data), "d" (port));
+  __asm__ volatile ("outb %0, %1" :: "a" (data), "d" (port));
 }
 
 static inline uint8_t
@@ -14,7 +14,7 @@ inb(uint16_t port)
 {
   uint8_t data;
 
-  __asm__ volatile ("in %1, %0" : "=r" (data) : "d" (port));
+  __asm__ volatile ("inb %1, %0" : "=a" (data) : "d" (port));
 
   return data;
 }
