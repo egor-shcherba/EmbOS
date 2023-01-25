7a1f84c9c9102a4a753f6d104e4e6cfa146418c2
Egor Shcherba <egorshcherba94@gmail.com>
2023-01-25 11:45:11 +0300

    main.c:: add uart_init

--- main.c
+++ main.c
@@ -2,7 +2,9 @@
 #include <sys/idt.h>
 #include <sys/pic.h>
 #include <mem/heap.h>
+#include <driver/uart.h>
 #include <debug/qemu.h>
+#include <system/printf.h>
 
 void
 main(void)
@@ -12,8 +14,10 @@ main(void)
   gdt_init();
   idt_init();
   pic_init();
-
   heap_init();
+  uart_init();
+
+  printf("Welcome to EmbOS...\n");
 
   for (;;)
     ;
