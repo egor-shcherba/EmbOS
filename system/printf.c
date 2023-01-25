91af61d75348da0121ce867092141b9b1c21567e
Egor Shcherba <egorshcherba94@gmail.com>
2023-01-25 11:43:25 +0300

    printf:: implemented

---
+++ system/printf.c
@@ -0,0 +1,24 @@
+#include <system/printf.h>
+#include <driver/uart.h>
+#include <klib/vsprintf.h>
+#include <sys/x86.h>
+
+#include <stdarg.h>
+
+#define BUFSIZE  4096
+
+void
+printf(const char *format, ...)
+{
+  char buf[BUFSIZE];
+  char *s = buf;
+  va_list ap;
+  int len = 0;
+
+  va_start(ap, format);
+  len = vsprintf(buf, format, ap);
+  va_end(ap);
+
+  while (len--)
+    uart_write(*s++);
+}
---
+++ system/printf.h
@@ -0,0 +1,6 @@
+#ifndef _PRINTF_H
+#define _PRINTF_H
+
+void printf(const char *format, ...);
+
+#endif /* NOT _PRINTF_H */
