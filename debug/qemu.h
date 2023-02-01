0786464e96e6d2d6086363584b321656165d0c57
Egor Shcherba <egorshcherba94@gmail.com>
2023-02-02 01:31:39 +0300

    qemu.h:: add debug macro

--- debug/qemu.h
+++ debug/qemu.h
@@ -1,6 +1,17 @@
 #ifndef _QEMU_H
 #define _QEMU_H
 
+#include <stddef.h>
 void qprintf(const char *fmt, ...);
+
+void dbg_printf(const char *header, const char *fmt, ...);
+
+#ifndef __DEBUG_HEADER__
+#define DEBUG(...)                           \
+  dbg_printf(__DEBUG_HEADER__, __VA_ARGS__)
+#else
+#define DEBUG(...)                           \
+  qprintf(__VA_ARGS__)
+#endif  /* __DEBUG_HEADER__ */
 
 #endif /* NOT QEMU_H */
