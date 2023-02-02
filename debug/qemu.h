2527d3a95088175da738076d42a003badc77226a
Egor Shcherba <egorshcherba94@gmail.com>
2023-02-02 12:41:45 +0300

    qemu.h :: rename DEBUG to dprintf

--- debug/qemu.h
+++ debug/qemu.h
@@ -7,10 +7,10 @@ void qprintf(const char *fmt, ...);
 void dbg_printf(const char *header, const char *fmt, ...);
 
 #ifndef __DEBUG_HEADER__
-#define DEBUG(...)                           \
+#define dprintf(...)                           \
   dbg_printf(__DEBUG_HEADER__, __VA_ARGS__)
 #else
-#define DEBUG(...)                           \
+#define dprintf(...)                           \
   qprintf(__VA_ARGS__)
 #endif  /* __DEBUG_HEADER__ */
 
