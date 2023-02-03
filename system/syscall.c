c5e40edc791354614b3155165e390f41d9e90178
Egor Shcherba <egorshcherba94@gmail.com>
2023-02-03 17:51:07 +0300

    syscall :: implemented

---
+++ system/syscall.c
@@ -0,0 +1,58 @@
+#include <sys/x86.h>
+#include <debug/qemu.h>
+#include <system/syscall.h>
+#include <system/sched.h>
+
+#define __DEBUG_HEADER__ "SYSCALL"
+
+static const char*
+get_syscall_name(int num)
+{
+  switch (num)
+    {
+      case SYS_thread_create : return "sys_thread_create";
+      case SYS_thread_yield  : return "sys_thread_yield";
+      case SYS_thread_sleep  : return "sys_thread_sleep";
+    }
+
+  return "UNKNOW";
+}
+
+int
+_syscall(int num, void *a1, void *a2, void *a3, void *a4, void *a5)
+{
+  int ret_val = 0;
+  __asm__ volatile
+  (
+    "movl %1, %%eax;"
+    "movl %2, %%ebx;"
+    "movl %3, %%ecx;"
+    "movl %4, %%edx;"
+    "movl %5, %%esi;"
+    "movl %6, %%edi;"
+    "int $0x80;"
+    : "=a" (ret_val)
+    : "a" (num), "b" (a1), "c" (a2), "d" (a3), "S" (a4), "D" (a5)
+  );
+
+  return ret_val;
+}
+
+int
+syscall_main(struct regs_t *regs)
+{
+  dprintf("call %d (%s) \n", regs->eax, get_syscall_name(regs->eax));
+
+  switch (regs->eax)
+    {
+      case SYS_thread_create: return sys_thread_create(
+        (void*) regs->ebx, (void*) regs->ecx,
+        (void*(*)(void*)) regs->edx, (void*) regs->edi);
+
+      case SYS_thread_yield: return sys_thread_yield();
+
+      case SYS_thread_sleep: return sys_thread_sleep();
+    }
+
+  return -1;
+}
---
+++ system/syscall.h
@@ -0,0 +1,10 @@
+#ifndef _SYSCALL_H
+#define _SYSCALL_H
+
+#define SYS_thread_create   0
+#define SYS_thread_yield    1
+#define SYS_thread_sleep    2
+
+int _syscall(int num, void *a1, void *a2, void *a3, void *a4, void *a5);
+
+#endif /* NOT _SYSCALL_H */
