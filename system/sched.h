448caa1a2e842289367a090918c4a987b5c91a47
Egor Shcherba <egorshcherba94@gmail.com>
2023-02-05 01:14:38 +0300

    sched.h :: add SYS_thread_wakeup

--- system/sched.h
+++ system/sched.h
@@ -6,12 +6,15 @@ struct thread;
 int sys_thread_create(
   struct thread **thread,
   const char *name,
-  void *(*start_routine)(void *arg),
+  void *(**start_routine)(void *arg),
   void *arg);
 
-int sys_thread_yeald(void);
+int sys_thread_yield(void);
 int sys_thread_sleep(void);
+int sys_thread_wakeup(struct thread *thread);
 
 void sched_init(void);
+
+void sched_enable(void);
 
 #endif /* NOT _SCHED_H */
