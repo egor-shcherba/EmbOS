d05a0e0b7d4810d034167552be0c6b871e25710d
Egor Shcherba <egorshcherba94@gmail.com>
2023-02-07 02:37:34 +0300

    init.c :: test arg

--- bin/init.c
+++ bin/init.c
@@ -5,40 +5,14 @@
 
 #include <debug/qemu.h>
 
-struct thread *t1, *t2;
-
-void*
-thread_2(void *arg)
-{
-  UNUSED(arg);
-
-  printf("thread 2\n");
-
-  thread_exit( (void*) -14);
-
-  return (void*) -2;
-}
+struct thread *t1;
 
 void*
 thread_1(void *arg)
 {
-  UNUSED(arg);
+  printf("thread 1 val %d\n", *(int*) arg);
 
-  printf("thread 1\n");
-
-  int retval = 0;
-  int t_arg = 256;
-
-  thread_create(&t2, "thread 2", &thread_2, (void*) &t_arg);
-
-  thread_join(t2, (void**) &retval);
-  printf("thread 2 exit retval %d\n", retval);
-
-
-  thread_exit( (void*) -24);
-  printf("thread 1 dead\n");
-
-  return (void*) 1;
+  return NULL;
 }
 
 void*
@@ -46,15 +20,10 @@ init(void *arg)
 {
   UNUSED(arg);
 
-  printf("init thread\n");
-  thread_create(&t1, "thread 1", &thread_1, NULL);
+  int val = 255;
 
-  int retval = 0;
-  thread_join(t1, (void**) &retval);
-  printf("thread 1 exit retval %d\n", retval);
-
-  printf("init thread dead\n");
-  thread_exit( (void*) 218);
+  thread_create(&t1, "thread 1", &thread_1, &val);
+  thread_join(t1, NULL);
 
   return NULL;
 }
