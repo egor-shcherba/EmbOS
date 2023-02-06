45a0642ae4e05bc28fc8bfcb8f3133574030d46a
Egor Shcherba <egorshcherba94@gmail.com>
2023-02-07 02:12:03 +0300

    sched.h :: define sys_thread_exit

--- system/sched.h
+++ system/sched.h
@@ -12,6 +12,8 @@ int sys_thread_create(
 int sys_thread_yield(void);
 int sys_thread_sleep(void);
 int sys_thread_wakeup(struct thread *thread);
+int sys_thread_join(struct thread *thread, void **arg);
+int sys_thread_exit(void *retval);
 
 void sched_init(void);
 
