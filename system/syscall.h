16a847b2979bde13c93de5b4a8fd42b3e08f7906
Egor Shcherba <egorshcherba94@gmail.com>
2023-02-07 02:11:09 +0300

    syscall.h :: add SYS_thread_exit

--- system/syscall.h
+++ system/syscall.h
@@ -5,6 +5,8 @@
 #define SYS_thread_yield    1
 #define SYS_thread_sleep    2
 #define SYS_thread_wakeup   3
+#define SYS_thread_join     4
+#define SYS_thread_exit     5
 
 int _syscall(int num, void *a1, void *a2, void *a3, void *a4, void *a5);
 
