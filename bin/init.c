595f3632b1c4e1eaf1d94786e1759ec70c2ef050
Egor Shcherba <egorshcherba94@gmail.com>
2023-02-07 13:59:31 +0300

    init :: test thread_self, thread_id

--- bin/init.c
+++ bin/init.c
@@ -12,6 +12,10 @@ thread_1(void *arg)
 {
   printf("thread 1 val %d\n", *(int*) arg);
 
+  struct thread *self = thread_self();
+  printf("thread self %.8x\n", self);
+  printf("thread id %d\n", thread_id());
+
   return NULL;
 }
 
