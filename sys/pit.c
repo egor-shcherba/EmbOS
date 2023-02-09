2f0d538c287316baae18460731f39267b19454cf
Egor Shcherba <egorshcherba94@gmail.com>
2023-02-09 23:49:07 +0300

    pit :: interface declaration and implementation pit_init

---
+++ sys/pit.c
@@ -0,0 +1,31 @@
+#include <sys/pit.h>
+#include <sys/pic.h>
+#include <sys/io.h>
+#include <debug/qemu.h>
+#include <system/sched.h>
+#include <stdint.h>
+
+#define PIT_FREQ      1193182
+
+#define PIT_CMD_PORT  0x43
+#define PIT_CHANNEL0  0x40
+
+#define __DEBUG_HEADER__  "PIT"
+
+static void
+pit_tick(void)
+{
+  schedule();
+}
+
+void
+pit_init(void)
+{
+  uint16_t latch = PIT_FREQ / HZ;
+
+  outb(PIT_CMD_PORT, 0x36);
+  outb(PIT_CHANNEL0, latch & 0xFF);
+  outb(PIT_CHANNEL0, latch >> 8);
+
+  irq_set_handler(0, pit_tick);
+}
---
+++ sys/pit.h
@@ -0,0 +1,8 @@
+#ifndef _PIT_H
+#define _PIT_H
+
+#define HZ 100
+
+void pit_init(void);
+
+#endif /* NOT _PIT_H */
