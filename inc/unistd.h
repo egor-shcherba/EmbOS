ea4a9e4589a7fbafa13517baa003859aa3076836
Egor Shcherba <egorshcherba94@gmail.com>
2023-02-16 13:27:30 +0300

    unistd.h :: add msleep

--- inc/unistd.h
+++ inc/unistd.h
@@ -9,4 +9,10 @@ sleep(unsigned seconds)
   return _syscall(SYS_settimer, (void*) (seconds * 1000), NULL, NULL, NULL, NULL);
 }
 
+static inline int
+msleep(unsigned mseconds)
+{
+  return _syscall(SYS_settimer, (void*) (mseconds), NULL, NULL, NULL, NULL);
+}
+
 #endif  /* NOT _UNISTD_H */
