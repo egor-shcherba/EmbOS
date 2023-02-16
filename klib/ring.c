a16d713652b930f5d14546fa9f82c9a31c33ef83
Egor Shcherba <egorshcherba94@gmail.com>
2023-02-16 13:19:34 +0300

    ring buffer interface and implementation

---
+++ klib/ring.c
@@ -0,0 +1,41 @@
+#include <mem/heap.h>
+#include <klib/ring.h>
+
+void
+ring_buffer_init(struct ring_buffer *rb, int capacity)
+{
+  rb->capacity = capacity;
+  rb->buffer = kmalloc(sizeof(char) * capacity);
+  rb->tail = 0;
+  rb->head = 0;
+  rb->size = 0;
+}
+
+int
+ring_buffer_write(struct ring_buffer *rb, char c)
+{
+  if (ring_buffer_is_full(rb))
+    return -1;
+
+  rb->buffer[rb->tail++ % rb->capacity] = c;
+  rb->size++;
+  return c;
+}
+
+int
+ring_buffer_read(struct ring_buffer *rb)
+{
+  if (ring_buffer_is_empty(rb))
+    return -1;
+
+  rb->size--;
+  return rb->buffer[rb->head++ % rb->capacity];
+}
+
+void
+ring_buffer_flush(struct ring_buffer *rb)
+{
+  rb->size = 0;
+  rb->head = 0;
+  rb->tail = 0;
+}
---
+++ klib/ring.h
@@ -0,0 +1,35 @@
+#ifndef _RING_H
+#define _RING_H
+
+struct ring_buffer {
+  int capacity;
+  char *buffer;
+  int tail;
+  int head;
+  int size;
+};
+
+static inline int
+ring_buffer_is_full(struct ring_buffer *rb)
+{
+  if (rb->size >= rb->capacity)
+    return 1;
+
+  return 0;
+}
+
+static inline int
+ring_buffer_is_empty(struct ring_buffer *rb)
+{
+  if (rb->head == rb->tail)
+    return 1;
+
+  return 0;
+}
+
+void ring_buffer_init(struct ring_buffer *rb, int capacity);
+int ring_buffer_write(struct ring_buffer *rb, char c);
+int ring_buffer_read(struct ring_buffer *rb);
+void ring_buffer_flush(struct ring_buffer *rb);
+
+#endif /* NOT _RING_H */
