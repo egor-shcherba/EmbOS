4cb77183b1a3978c39df16921047ca91f4579d0c
Egor Shcherba <egorshcherba94@gmail.com>
2023-02-16 13:26:01 +0300

    stdio.h :: add BUFSIZE, getchar, gets

--- inc/stdio.h
+++ inc/stdio.h
@@ -4,8 +4,11 @@
 #include <driver/uart.h>
 
 #define putchar(x)  uart_write(x)
+#define BUFSIZ      4096
 
 void printf(const char *format, ...);
 
+int getchar(void);
+char *gets(char *s);
 
 #endif /* NOT _STDIO_H */
