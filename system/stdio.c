e4acc90ad20a8df11460a226d1f0e19ed4d3e180
Egor Shcherba <egorshcherba94@gmail.com>
2023-02-21 01:34:08 +0300

    stdio :: add scanf

--- system/stdio.c
+++ system/stdio.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <stddef.h>
+#include <stdarg.h>
+#include <klib/vsprintf.h>
+#include <klib/vsscanf.h>
 #include <system/syscall.h>
 
 int
@@ -28,8 +31,47 @@ gets(char *s)
   return s;
 }
 
+int
+scanf(const char *format, ...)
+{
+  char buf[BUFSIZ];
+  int err = 0;
+  va_list ap;
+
+  gets(buf);
+
+  va_start(ap, format);
+  err = vsscanf(buf, format, ap);
+  va_end(ap);
+
+  return err;
+}
+
 void
 fflush(void)
 {
   _syscall(SYS_flush, NULL, NULL, NULL, NULL, NULL);
+}
+
+void
+puts(const char *s)
+{
+  while (*s)
+    putchar(*s++);
+}
+
+void
+printf(const char *format, ...)
+{
+  char buf[BUFSIZ];
+  char *s = buf;
+  va_list ap;
+  int len = 0;
+
+  va_start(ap, format);
+  len = vsprintf(buf, format, ap);
+  va_end(ap);
+
+  while (len--)
+    uart_write(*s++);
 }
