f3867b6d8fad4a3a81cc01f7d3c3dfe40edae50f
Egor Shcherba <egorshcherba94@gmail.com>
2023-02-21 01:35:08 +0300

    stdio.h :: add scanf

--- inc/stdio.h
+++ inc/stdio.h
@@ -4,11 +4,14 @@
 #include <driver/uart.h>
 
 #define putchar(x)  uart_write(x)
+
 #define BUFSIZ      4096
 
 void printf(const char *format, ...);
+void puts(const char *s);
 
 int getchar(void);
 char *gets(char *s);
+int scanf(const char *format, ...);
 
 #endif /* NOT _STDIO_H */
